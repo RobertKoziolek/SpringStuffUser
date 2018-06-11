67b5ebf41079d3683120d291b352dd67519a6c43
Robert Koziolek <robertkoziolek4c@gmail.com>
2018-06-11 09:43:29 +0200

    initial commit

---
+++ src/main/kotlin/com/robcio/springstuff/SpringstuffApplication.kt
@@ -0,0 +1,11 @@
+package com.robcio.springstuff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringstuffApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringstuffApplication>(*args)
+}
